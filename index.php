6d9f01fd9ffce37d4423d717f1ed1bf51f29c104
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-12-16 15:30:59 +0100

    add 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Descrizione:
+    Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza. Una parola da censurare viene passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+    
+<?php 
+
+$text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. In nihil necessitatibus aut doloribus aliquam totam laborum. Porro vel, minus molestiae qui officiis, quis aspernatur sint quos tenetur recusandae enim tempore!';
+echo 'FRASE COMPLETA:'.'<br><br>';
+echo $text .'<br><br>';
+echo 'Lunghezza del testo:';
+echo strlen($text);
+
+?>
+</body>
+</html>
